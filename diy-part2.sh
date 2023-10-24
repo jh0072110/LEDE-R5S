52ed04a2b24359d704eecf6c4e284d65213de0ca
BigBB2110 <50496854+jh0072110@users.noreply.github.com>
2023-10-25 03:53:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,7 @@
 # Modify default IP
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-argon-mod
+rm -rf feeds/luci/applications/luci-app-argon-config
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
