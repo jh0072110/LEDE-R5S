0c303fb532172e3a25af13f17f5d00d4f0f93688
BigBB2110 <50496854+jh0072110@users.noreply.github.com>
2023-10-25 03:50:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,12 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon-mod
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
+git clone -b main https://github.com/xiaorouji/openwrt-passwall package/psw
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/pswcore
+git clone https://github.com/honwen/luci-app-aliddns package/aliddns
