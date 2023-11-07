cd357c698f67b397a2408019d44902c9c67eb599
BigBB2110 <50496854+jh0072110@users.noreply.github.com>
2023-11-07 22:26:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,6 +23,6 @@ git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/l
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
-git clone -b main https://github.com/jh0072110/openwrt-passwall package/psw
+git clone -b main https://github.com/xiaorouji/openwrt-passwall package/psw
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/pswcore
 git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
